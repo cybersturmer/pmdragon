e32a9bd0601430c83e620d69f48c001bd39d7648
Wladimir Sturmer <cybersturmer@ya.ru>
2020-05-17 21:38:04 +0300

    Renamed directory

--- api/conf/wsgi.py
+++ api/conf/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.conf.production.settings')
 
 application = get_wsgi_application()
