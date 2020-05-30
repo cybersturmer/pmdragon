8755fe40689eafb96ff196967a82bcfca6d8d635
Wladimir Sturmer <cybersturmer@ya.ru>
2020-05-30 07:19:39 +0300

    Docker-compose fixes

--- api/conf/asgi.py
+++ api/conf/asgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'back.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'conf.development.settings')
 
 application = get_asgi_application()
