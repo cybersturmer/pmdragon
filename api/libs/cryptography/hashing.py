13472c55ec3e8f3119a4f19a0d5a33c44ee5875c
Wladimir Sturmer <cybersturmer@ya.ru>
2020-06-25 20:43:02 +0300

    Small spaces removed

--- api/libs/cryptography/hashing.py
+++ api/libs/cryptography/hashing.py
@@ -6,5 +6,3 @@ from django.conf import settings
 def get_hash(raw_string: str) -> str:
     raw_string += settings.SECRET_KEY
     return hashlib.sha3_256(raw_string.encode('utf-8')).hexdigest()
-
-
