716a154eb99927109010a7982bd5b6c58131b45c
Wladimir Sturmer <cybersturmer@ya.ru>
2020-08-08 20:37:52 +0300

    Workspace predefining by project field.

--- api/apps/core/admin.py
+++ api/apps/core/admin.py
@@ -30,7 +30,6 @@ class WorkspaceAdmin(admin.ModelAdmin):
 class IssueStateAdmin(admin.ModelAdmin):
     model = IssueStateCategory
     list_display = (
-        'workspace',
         'project',
         'title',
         'is_default',
@@ -43,7 +42,6 @@ class IssueStateAdmin(admin.ModelAdmin):
 class IssueTypeAdmin(admin.ModelAdmin):
     model = IssueTypeCategory
     list_display = (
-        'workspace',
         'project',
         'title',
         'is_subtask',
@@ -68,18 +66,43 @@ class SprintDurationAdmin(admin.ModelAdmin):
 class SprintAdmin(admin.ModelAdmin):
     model = Sprint
     list_display = (
-        'workspace',
         'project',
         'title',
         'goal',
         'started_at',
         'finished_at'
     )
+    readonly_fields = ('workspace',)
     save_as = True
+
+    def save_model(self, request, obj, form, change):
+        obj.workspace = obj.project.workspace
+        super(SprintAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Issue)
+class IssueAdmin(admin.ModelAdmin):
+    model = Issue
+    list_display = (
+        'project',
+        'title',
+        'type_category',
+        'state_category',
+        'created_by'
+    )
+    readonly_fields = (
+        'workspace',
+    )
+    search_fields = (
+        'title'
+    )
+    
+    def save_model(self, request, obj, form, change):
+        obj.workspace = obj.project.workspace
+        super(IssueAdmin, self).save_model(request, obj, form, change)
 
 
 admin.site.register(Project)
-admin.site.register(Issue)
 admin.site.register(ProjectBacklog)
 admin.site.unregister(User)
 admin.site.register(User, UserAdmin)
