3591b838fc5f338d4524317392c95f8e73f054c9
Wladimir Sturmer <cybersturmer@ya.ru>
2020-08-16 02:22:27 +0300

    Updated component for header.

---
+++ client/src/components/BlockHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <h5>{{ title }}</h5>
+</template>
+
+<script>
+export default {
+  name: 'BlockHeader',
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h5 {
+    text-transform: uppercase;
+  }
+</style>
---
+++ client/src/components/BlockHeaderInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <h5>
+    {{ title }}
+    <span>
+      (&nbsp;{{ info }}&nbsp;)
+    </span>
+  </h5>
+</template>
+
+<script>
+export default {
+  name: 'BlockHeaderInfo',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    info: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h5 {
+    text-transform: uppercase;
+  }
+  span {
+    font-size: 0.75em;
+    text-transform: none;
+  }
+</style>
