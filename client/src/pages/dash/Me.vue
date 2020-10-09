9289161324a3d76dbb473199e7c69a5f4590d651
Wladimir Sturmer <cybersturmer@ya.ru>
2020-10-09 16:12:39 +0300

    We can save user data on Me page.

--- client/src/pages/dash/Me.vue
+++ client/src/pages/dash/Me.vue
@@ -1,9 +1,45 @@
 <template>
   <q-page class="flex q-layout-padding">
     <div class="column full-width">
-      <div class="row">First name {{ firstName }}</div>
-      <div class="row">Last name {{ lastName }}</div>
-      <div class="row">User name {{ userName }}</div>
+      <div class="row">
+        <q-card dark bordered class="bg-grey-9 my-card">
+          <q-card-section>
+            <div class="text-h6 text-center">Main user Data</div>
+            <q-input
+              dark
+              dense
+              square
+              outlined
+              v-model="user_form_data.firstName"
+              label="First name"
+              class="q-mb-sm"
+            />
+            <q-input
+              dark
+              dense
+              square
+              outlined
+              v-model="user_form_data.lastName"
+              label="Last name"
+              class="q-mb-sm"
+            />
+            <q-input
+              dark
+              dense
+              square
+              outlined
+              v-model="user_form_data.userName"
+              label="Username"
+            />
+          </q-card-section>
+
+          <q-card-actions vertical>
+            <q-btn flat @click="saveUserData">
+              Update
+            </q-btn>
+          </q-card-actions>
+        </q-card>
+      </div>
     </div>
   </q-page>
 </template>
@@ -11,30 +47,24 @@
 <script>
 export default {
   name: 'AccountView',
-  computed: {
-    firstName: {
-      get: function () {
-        return this.$store.getters['auth/FIRST_NAME']
-      },
-      set: function () {
-        /** @todo Add mutation and action */
+  data () {
+    return {
+      user_form_data: {
+        firstName: this.$store.getters['auth/FIRST_NAME'],
+        lastName: this.$store.getters['auth/LAST_NAME'],
+        userName: this.$store.getters['auth/USERNAME']
       }
-    },
-    lastName: {
-      get: function () {
-        return this.$store.getters['auth/LAST_NAME']
-      },
-      set: function () {
-        /** @todo Add mutation and action */
+    }
+  },
+  methods: {
+    saveUserData () {
+      const payload = {
+        first_name: this.user_form_data.firstName,
+        last_name: this.user_form_data.lastName,
+        username: this.user_form_data.userName
       }
-    },
-    userName: {
-      get: function () {
-        return this.$store.getters['auth/USERNAME']
-      },
-      set: function () {
-        /** @todo Add mutation and action */
-      }
+
+      this.$store.dispatch('auth/USER_UPDATE', payload)
     }
   }
 }
