2f4fe645cad51f2e98ce3dbc731e5682bffd69fa
Wladimir Sturmer <cybersturmer@ya.ru>
2020-10-09 15:08:38 +0300

    State for username was added.

--- client/src/store/auth/state.js
+++ client/src/store/auth/state.js
@@ -3,6 +3,7 @@ import { LocalStorage, SessionStorage } from 'quasar'
 export default function () {
   return {
     user_id: LocalStorage.getItem('auth.user_id') || null,
+    username: LocalStorage.getItem('auth.username') || null,
     first_name: LocalStorage.getItem('auth.first_name') || null,
     last_name: LocalStorage.getItem('auth.last_name') || null,
     tokens: {
