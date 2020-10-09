d8e8013158f02593b47da259b780a021dc1656dc
Wladimir Sturmer <cybersturmer@ya.ru>
2020-10-09 15:43:14 +0300

    Save user data action was added.

--- client/src/store/auth/actions.js
+++ client/src/store/auth/actions.js
@@ -1,4 +1,6 @@
 import { AuthService } from 'src/services/auth'
+import { Api } from 'src/services/api'
+import { ErrorWrapper, HandleResponse } from 'src/services/util'
 
 export async function LOGIN ({ commit }, credentials) {
   return await AuthService.login(credentials)
@@ -10,4 +12,19 @@ export async function REFRESH ({ commit }, refreshToken) {
 
 export async function LOGOUT ({ dispatch }) {
   await AuthService.logout()
+}
+
+export async function USER_UPDATE ({ commit }, payload) {
+  try {
+    const response = await new Api({ auth: true })
+      .put('/auth/me/', payload)
+
+    HandleResponse.compare(200, response.status)
+
+    commit('SET_FIRST_NAME', response.data.first_name)
+    commit('SET_LAST_NAME', response.data.last_name)
+    commit('SET_USERNAME', response.data.username)
+  } catch (error) {
+    throw new ErrorWrapper(error)
+  }
 }
