fc15df889e4092926c4ea57b6798f868574e5698
Wladimir Sturmer <cybersturmer@ya.ru>
2020-10-11 22:25:25 +0300

    Avatar added to obtain pair method.

--- api/apps/core/api/serializers.py
+++ api/apps/core/api/serializers.py
@@ -73,6 +73,7 @@ class TokenObtainPairExtendedSerializer(serializers_jwt.TokenObtainPairSerialize
             'username': self.user.username,
             'first_name': self.user.first_name,
             'last_name': self.user.last_name,
+            'avatar': self.user.person.avatar,
             'tokens': {
                 'access': access_token,
                 'refresh': refresh_token,
