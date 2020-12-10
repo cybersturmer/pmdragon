403c19ad32d14078cf9695bc5bc85c611ecfc0b6
Vladimir Shturmer <cybersturmer@ya.ru>
2020-12-11 00:20:10 +0300

    Refactored.

--- client/src/.env.js
+++ client/src/.env.js
@@ -2,10 +2,10 @@ export const DEBUG = true
 
 export const DEBUG_ENV = {
   port: 443,
-  url: `https://localhost:443/api`
+  url: 'https://localhost:443/api'
 }
 
 export const PROD_ENV = {
   port: 8000,
-  url: `http://localhost:8000/api`
+  url: 'http://localhost:8000/api'
 }
