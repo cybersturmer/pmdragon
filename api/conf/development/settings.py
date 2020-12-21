1d4764d3df3df845f39a53d31ae05571f14a125f
Vladimir Shturmer <cybersturmer@ya.ru>
2020-12-22 00:11:42 +0300

    access token lifetime increased to 15 minutes.

--- api/conf/development/settings.py
+++ api/conf/development/settings.py
@@ -30,7 +30,7 @@ REST_FRAMEWORK.update({
 })
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=6),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
