5984b7a6e79e58ba8893e8f7391d377b10cdcc7d
Vladimir Shturmer <cybersturmer@ya.ru>
2021-06-09 23:57:19 +0300

    store to keystore

--- scripts/mobile/android/sign.sh
+++ scripts/mobile/android/sign.sh
@@ -1,2 +1,2 @@
 # By this we can sign unsigned apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore pmdragon app-release-unsigned.apk pmdragon
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore pmdragon.keystore app-release-unsigned.apk pmdragon
