76faffef6c489dbc31470729ae46c7f52737cfd8
Vladimir Shturmer <cybersturmer@ya.ru>
2021-06-14 23:01:56 +0300

    Scripts updated to be able launched with my folder structure.

--- scripts/mobile/android/optimize.sh
+++ scripts/mobile/android/optimize.sh
@@ -1,2 +1,5 @@
 # Let's optimize our APK before attaching it to Google Play
-zipalign -v 4 app-release-unsigned.apk app-release-signed.apk
+FRONTEND_PATH=~/projects/pmdragon/pmdragon-client
+APK_BUILD_PATH=$FRONTEND_PATH/dist/cordova/android/apk/release
+
+zipalign -v 4 $APK_BUILD_PATH/app-release-unsigned.apk $APK_BUILD_PATH/app-release-signed.apk
--- scripts/mobile/android/sign.sh
+++ scripts/mobile/android/sign.sh
@@ -1,2 +1,5 @@
 # By this we can sign unsigned apk
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore pmdragon.keystore app-release-unsigned.apk pmdragon
+FRONTEND_PATH=~/projects/pmdragon/pmdragon-client
+APK_BUILD_PATH=$FRONTEND_PATH/dist/cordova/android/apk/release
+
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $FRONTEND_PATH/pmdragon.keystore $APK_BUILD_PATH/app-release-unsigned.apk pmdragon
