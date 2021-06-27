f3ca3454a18131ab7adc766414bca92e323bf204
Vladimir Shturmer <cybersturmer@ya.ru>
2021-06-28 02:59:04 +0300

    We need just one shell to generate signed and optimized APK

--- scripts/mobile/android/optimize.sh
+++
@@ -1,5 +0,0 @@
-# Let's optimize our APK before attaching it to Google Play
-FRONTEND_PATH=~/projects/pmdragon/pmdragon-client
-APK_BUILD_PATH=$FRONTEND_PATH/dist/cordova/android/apk/release
-
-zipalign -v 4 $APK_BUILD_PATH/app-release-unsigned.apk $APK_BUILD_PATH/app-release-signed.apk
--- scripts/mobile/android/sign.sh
+++
@@ -1,5 +0,0 @@
-# By this we can sign unsigned apk
-FRONTEND_PATH=~/projects/pmdragon/pmdragon-client
-APK_BUILD_PATH=$FRONTEND_PATH/dist/cordova/android/apk/release
-
-jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $FRONTEND_PATH/pmdragon.keystore $APK_BUILD_PATH/app-release-unsigned.apk pmdragon
