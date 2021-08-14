ddff0e40748f4c9f9c6eb1e5a4f5e4d5a3829ca3
Vladimir Shturmer <cybersturmer@ya.ru>
2021-08-15 02:24:09 +0300

    Add files before zipping.

--- scripts/electron/generate.sh
+++ scripts/electron/generate.sh
@@ -9,8 +9,19 @@ cd $FRONTEND_PATH || exit 0
 quasar build --mode electron --target all --arch all
 
 cd $BUILD_PATH || exit 0
-rm -R UnPackaged # We don't need to pack this directory
+
+# We don't need to pack this directory
+rm -R UnPackaged
 
 for directory in */ ; do
+  # Copy Install instruction to folder before zip
+  if [[ $directory == *"linux"* ]]; then
+    cp ../../Install-Linux-tar.txt "$BUILD_PATH/$directory"
+  fi
+
+  # Copy LICENSE file to folder before zip it
+  cp ../../LICENSE "$BUILD_PATH/$directory"
+
+  # Zip folder with original name
   zip -r "${directory%/}.zip"  "$BUILD_PATH/$directory"
 done
