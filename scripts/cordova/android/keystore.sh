2fb206f9d24172022d30ed95acaaf37218c69f29
Vladimir Shturmer <cybersturmer@ya.ru>
2021-11-14 00:14:13 +0300

    keytool docs

--- scripts/cordova/android/keystore.sh
+++ scripts/cordova/android/keystore.sh
@@ -1,2 +1,3 @@
 # Generating key for signing android application
-keytool -genkey -v -keystore pmdragon.keystore -alias pmdragon -keyalg RSA -keysize 2048 -validity 10000
+# https://docs.oracle.com/javase/8/docs/technotes/tools/unix/keytool.html
+keytool -genkeypair -v -keystore pmdragon.keystore -alias pmdragon -keyalg RSA -keysize 2048 -validity 10000
